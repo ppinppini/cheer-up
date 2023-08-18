b1704e2e294e0bfad79d240e1d1aaac05fa740a4
Ppinppini <bnm0218@naver.com>
2023-08-18 14:58:24 +0900

    third commit

--- slideShow.js
+++ slideShow.js
@@ -1,6 +1,9 @@
 const images = document.querySelectorAll(".image");
 const buttons = document.querySelectorAll(".button");
 
+let currentIndex = 0;
+const slideInterval = 3000; // 이미지가 넘어가는 간격 (밀리초)
+
 function changeImage(index) {
   images.forEach((image) => image.classList.remove("selected"));
   buttons.forEach((button) => button.classList.remove("selected"));
@@ -8,8 +11,22 @@ function changeImage(index) {
   images[index].classList.add("selected");
   buttons[index].classList.add("selected");
 
-  const translateX = index === 0 ? 0 : index === 1 ? -258 : -538;
+  const imageWidth = images[0].clientWidth;
+  const translateX = index === 0 ? 0 : index === 1 ? -340: -680;
   document.querySelector(
     ".image-wrapper"
   ).style.transform = `translateX(${translateX}px)`;
+
+  currentIndex = index;
 }
+
+function autoSlide() {
+  currentIndex = (currentIndex + 1) % images.length;
+  changeImage(currentIndex);
+}
+
+// 초기 이미지 표시
+changeImage(0);
+
+// 일정 시간 간격으로 자동 슬라이딩
+setInterval(autoSlide, slideInterval);
